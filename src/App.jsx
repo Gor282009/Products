8a221f8e20e4740e4dc5598226c6ed68480e7504
Gor282009 <vardanyangor46@gmail.com>
2024-08-12 23:46:13 +0400

    Products Added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div className="home-cont">
+        <h1 className='my-title'>Welcome to our Shop!</h1>
+        <p className='my-text'>Here, you can find our latest products.</p>
+        <button className='my-btn'>Discover Our Products</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "../../App.css"
+
 function About() {
+  const [data, SetData] = useState([]);
+  useEffect(() => {
+    fetch('https://dummyjson.com/products/search?q=phone')
+      .then((res) => res.json())
+      .then((res) => SetData(res.products));
+  }, []);
   return (
-    <div>
-      <h1>About</h1>
+    <div className="home1">
+      {data.map((e) => {
+        return (
+          <div key={e.id} className="box1">
+            <img src={e.images[0]} alt=""  className="pictures1"/>
+            <h2 className="title1">{e.title}</h2>
+            <p>{e.brand}</p>
+            <p>{e.category}</p>
+            <p>{e.description}</p>
+            <p>{e.price} $</p>
+            <button className="add-button1">Add to Cart</button>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
